1a5c614d3193ce162da294fd3c0d2c1f293a2874
kadiralev1 <38442381+kadiralev1@users.noreply.github.com>
2018-08-23 15:00:16 +0300

    algorithm

--- Contents.swift
+++ Contents.swift
@@ -5,8 +5,13 @@ import UIKit
 // ı want explain the fizbuzz algorithm we assume have a number of series for example 1-2-3-4-5....15 like that . if number can divided by 3 the system should write fizz if number can divided by 5 system should write buzz . finally if number can divided by both 3 and 5 system should write fizzbuzz sorry for my bad english :)
 
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+var oneThousandNumbers = [Int]()
 
-for num in numbers {
+for i in 1...1000 {
+    oneThousandNumbers.append(i)
+}
+
+for num in oneThousandNumbers {
     
     if num % 3 == 0 {
         print("\(num) fizz")
